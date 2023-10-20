345c65ae46d040402f9f677291a0d52a7f83c6cb
Hary S <hary.samardzic@gmail.com>
2023-10-20 13:04:03 +0200

    prisma setup complete

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,19 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
 
+import prisma from './src/lib/prisma';
+
 //For env File 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
+app.get('/', async (req: Request, res: Response) => {
+  const data = await prisma.user.findMany();
+  res.send(data)
 });
+
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
